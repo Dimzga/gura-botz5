b6cbc547571a89f19e784601f936a08c6e71c3e6
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-09-20 20:55:23 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,19 +1,17 @@
-import { tiktokdl } from '@bochilteam/scraper'
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*TIKTOK DOWNLOADER*
-*Nickname:* ${nickname}
-*Description:* ${description}
-
-_©Sad-Bot_
-`.trim(), m)
+const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
+    tiktokdlv3(args[0]).then(r => {
+    let video = r.video.no_watermark
+    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
+    })
 }
-handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+handler.limit = true
+handler.group = true
 
-export default handler
+handler.command = /^(tt|tiktok|tik)$/i
+
+module.exports = handler
+
